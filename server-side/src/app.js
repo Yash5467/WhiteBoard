34ddaa3ab71643e03ca09cab40672173d9bf4ad3
Yashpal Chouhan <yashstar0009@gmail.com>
2024-01-09 12:21:54 +0530

    Response Headers Updated

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -6,6 +6,13 @@ const app=express();
 
 
 app.use(cors());
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Region","*");
+    res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type,Accept,Authorization");
+
+    next();
+})
 app.use(express.json({limit : '16kb'}));
 
 app.use(express.urlencoded({extended: true, limit :'16kb'}));
